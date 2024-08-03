4e20b176b7f92ea39b6353a848b118e15e186336
Camila <alvescamila87@gmail.com>
2024-08-02 22:22:10 -0300

    query methods

--- src/main/java/br/com/camila/projetoempresa/dao/DepartamentoDAO.java
+++ src/main/java/br/com/camila/projetoempresa/dao/DepartamentoDAO.java
@@ -1,5 +1,7 @@
 package br.com.camila.projetoempresa.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.camila.projetoempresa.model.Departamento;
@@ -7,5 +9,17 @@ import br.com.camila.projetoempresa.model.Departamento;
 public interface DepartamentoDAO extends CrudRepository<Departamento, Integer>{
 	
 	// consultas customizadas
+	
+	public List<Departamento> findByOrderByNome();
+	
+	public List<Departamento> findByAndar(Integer andar);
+	
+	public List<Departamento> findByOrderByNomeDesc();
+	
+	public List<Departamento> findByOrderByNumeroIdDesc();
+	
+	// public List<Departamento> findByNumeroIdAndAndar(Integer numeroId, Integer andar);
+	
+	public List<Departamento> findByNumeroIdOrAndar(Integer numeroId, Integer andar);
 
 }
